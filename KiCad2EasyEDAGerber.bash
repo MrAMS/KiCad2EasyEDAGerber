64d9f4c0f63a19516388a2c24d8605d131952a6f
Santiego <2421653893@qq.com>
2024-12-16 17:47:07 +0800

    init

---
+++ KiCad2EasyEDAGerber.bash
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <production_dir>"
+    exit 1
+fi
+
+WORK_DIR="$1"
+
+TMP_DIR="$WORK_DIR/tmp"
+OUT_NAME="Gerber_PCB1_$(date +"%Y-%m-%d")"
+OUT_DIR="$WORK_DIR/$OUT_NAME"
+OUT_ZIP="$WORK_DIR/$OUT_NAME.zip"
+
+EasyEDA_VERSION="EasyEDA Pro v2.2.32.3"
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+
+# 函数：替换文件头部信息并重命名文件
+replace_header_and_rename() {
+  local regex="$1"
+  local rule="$2"
+  
+  # 查找符合正则表达式的文件
+  for file in $(find $TMP_DIR -type f -regextype posix-extended -regex ".*/$regex"); do
+    
+    local new_name=$(basename "$file" | sed -E "$rule")   
+    local layername=$(echo "$new_name" | sed -E 's/^Gerber_([^.]+)\..*$/\1/')
+    echo $file
+    echo $new_name
+    echo $layername
+    if grep -qE "^G04" "$file" ; then
+      echo "Gerber File"
+      sed -i '/^G04/d' "$file"
+      echo "G04 Layer: $layername*
+G04 $EasyEDA_VERSION, $DATE*
+G04 Gerber Generator version 0.3*
+G04 Scale: 100 percent, Rotated: No, Reflected: No*
+G04 Dimensions in millimeters*
+G04 Leading zeros omitted, absolute positions, 3 integers and 5 decimals*" | cat - "$file" > temp && mv temp "$file"
+    else
+      echo "DRL File"
+      local plated_type="PLATED"
+      if [[ $regex == *"NPTH"* ]]; then
+        plated_type="NON_PLATED"
+      fi
+      echo ";TYPE=$plated_type
+;Layer: $layername
+;$EasyEDA_VERSION, $DATE
+;Gerber Generator version 0.3" | cat - "$file" > temp && mv temp "$file"
+    fi
+
+    # 重命名文件
+    mv "$file" "$OUT_DIR/$new_name"
+  done
+}
+
+declare -A rename_rules=(
+# 钻孔
+  [".*\-NPTH\.drl"]="s/.*\-NPTH\.drl$/Drill_NPTH_Through.DRL/"
+  [".*\-PTH\.drl"]="s/.*\-PTH\.drl$/Drill_PTH_Through.DRL/"
+# 机械层
+  [".*\.gm([0-9]+)$"]="s/.*\.gm([0-9]+)$/Gerber_MechanicalLayer\1.GM\1/"
+# 顶层
+  [".*\.gtl"]="s/.*\.gtl$/Gerber_TopLayer.GTL/"
+  [".*\.gtp"]="s/.*\.gtp$/Gerber_TopPasteMaskLayer.GTP/"
+  [".*\.gto"]="s/.*\.gto$/Gerber_TopSilkscreenLayer.GTO/"
+  [".*\.gts"]="s/.*\.gts$/Gerber_TopSolderMaskLayer.GTS/"
+# 中间层
+  [".*\.gp([0-9]+)$"]="s/.*\.gp([0-9]+)$/Gerber_InnerLayer\1.GP\1/"
+  [".*\.g([0-9]+)$"]="s/.*\.g([0-9]+)$/Gerber_InnerLayer\1.G\1/"
+# 底层
+  [".*\.gbl"]="s/.*\.gbl$/Gerber_BottomLayer.GBL/"
+  [".*\.gbo"]="s/.*\.gbo$/Gerber_BottomSilkscreenLayer.GBO/"
+  [".*\.gbs"]="s/.*\.gbs$/Gerber_BottomSolderMaskLayer.GBS/"
+)
+
+rm -r $TMP_DIR
+rm -r $OUT_DIR
+rm $OUT_ZIP
+mkdir $TMP_DIR
+mkdir $OUT_DIR
+echo "WORKING IN $OUT_DIR ..."
+unzip -o $WORK_DIR/*.zip -d $TMP_DIR
+
+# 应用所有重命名规则
+for regex in "${!rename_rules[@]}"; do
+  replace_header_and_rename "$regex" "${rename_rules[$regex]}"
+done
+
+echo "How to Order PCB
+
+Please refer to:
+https://prodocs.lceda.cn/cn/pcb/order-order-pcb/index.html" > $OUT_DIR/How-to-order-PCB.txt
+
+zip -r -j $OUT_ZIP $OUT_DIR/*
+
+rm -r $TMP_DIR
+rm -r $OUT_DIR
+
+echo "DONE. CHECK $OUT_ZIP."
+
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# KiCad2EasyEDAGerber 
+
+## Usage
+
+Use [JLC-Plugin-for-KiCad](https://github.com/bennymeg/JLC-Plugin-for-KiCad) to export production and then run script with that production directory path
+
+```bash
+./KiCad2EasyEDAGerber.bash /the/path/of/production
+```
+
+Verified available on 2024-12-16
